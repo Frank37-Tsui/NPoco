2de23f1b5ce79d4e243bee786fc61c2bd7a53b48
FrankTsui <Frank_Tsai@dynamicpcb.com.tw>
2020-01-20 16:16:35 +0800

    Add DatabaseExt

---
+++ src/NPoco/DatabaseExt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace NPoco
+{
+    public partial class Database
+    {
+#if !NETSTANDARD1_3
+        public DataTable ExecuteDataTable(string sql)
+        {
+            try
+            {
+                OpenSharedConnectionInternal();
+                using (var cmd = CreateCommand(_sharedConnection, CommandType.Text, sql))
+                {
+                    var dr = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(dr);
+                    return dt;
+                }
+            }
+            catch (Exception x)
+            {
+                OnExceptionInternal(x);
+                throw;
+            }
+            finally
+            {
+                CloseSharedConnectionInternal();
+            }
+        }
+#endif
+    }
+
+}
